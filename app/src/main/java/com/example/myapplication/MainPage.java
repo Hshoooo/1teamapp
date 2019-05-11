3c6f03f487f802bb9220dae8c733e038fdedd90b
sojeong <xhakxh1348@naver.com>
2019-05-12 00:40:38 +0900

    Mainpage test

--- app/src/main/java/com/example/myapplication/MainPage.java
+++ app/src/main/java/com/example/myapplication/MainPage.java
@@ -4,7 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainPage extends AppCompatActivity {
-
+    //냐하
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
