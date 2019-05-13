4503c7d1d76bd86142a1d279aa10046af4cc28af
sojeong <xhakxh1348@naver.com>
2019-05-13 11:13:55 +0900

    food1

--- app/src/main/java/com/example/myapplication/MainPage.java
+++ app/src/main/java/com/example/myapplication/MainPage.java
@@ -8,7 +8,7 @@ public class MainPage extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.mainmenu);
     }
 }
 
