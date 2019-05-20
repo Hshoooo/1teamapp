87a21082ab475fe1a05d127a3a87d2da86c9233a
sojeong <xhakxh1348@naver.com>
2019-05-20 13:49:30 +0900

    friend

---
+++ app/src/main/java/com/example/myapplication/Food.java
@@ -0,0 +1,13 @@
+package com.example.myapplication;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class Food extends AppCompatActivity {
+    //냐하
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.food);
+    }
+}
